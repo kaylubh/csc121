fdb1aeda52ccb47534fd663ef608669439f5dcbb
Caleb <calebhemp@gmail.com>
2026-01-14 22:35:48 -0500

    Update Lab01P4.py

--- labs/Lab01/Lab01P4.py
+++ labs/Lab01/Lab01P4.py
@@ -9,6 +9,7 @@
 book_price = 2.25
 dvd_price = 4.35
 game_price = 5.00
+sales_tax = 0.065
 
 # Input
 # gets number of each item being purchased
@@ -19,7 +20,13 @@ num_games = int(input('Games: '))
 
 # Processing
 # totals the order purchase price
-
+subtotal = (num_books * book_price) + (num_dvds * dvd_price) + (num_games * game_price)
+tax = subtotal * sales_tax
+total = subtotal + tax
 
 # Output
 # outputs totals to user
+print('--------------------')
+print(f'Subtotal: ${subtotal:.2f}')
+print(f'Tax: ${tax:.2f}')
+print(f'Total: ${total:.2f}')
